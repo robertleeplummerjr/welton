2a0591cde359f73e1982a89f32b6b12c9a8e1339
Robert Plummer <robertleeplummerjr@gmail.com>
2020-11-13 14:00:05 -0500

    fix: Check for window

--- src/components/app.js
+++ src/components/app.js
@@ -88,8 +88,8 @@ export const App = () => {
 			position: 'fixed',
 			background: 'url(/assets/irish-pub.png) no-repeat center center fixed',
 			backgroundSize: 'cover',
-			width: window.innerWidth,
-			height: window.innerHeight,
+			width: window ? window.innerWidth : 0,
+			height: window ? window.innerHeight : 0,
 			zIndex: 2,
 			fontColor: '#291f1b'
 		}}>
